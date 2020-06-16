a464e1a108c3401db0242a8eb064109891bed02d
Ethan <ebritton12321@gmail.com>
2020-06-16 01:03:31 -0400

    Added Ico Files Please Work

--- IndexJs/index.customlink.js
+++ IndexJs/index.customlink.js
@@ -2,7 +2,7 @@ var MyApp = angular.module('Index');
 
 MyApp.controller('customlink', ['$scope', '$interval', function($scope, $interval){
 
-    $scope.Pics = ['img/favicons/favicon1.jpg?', 'img/favicons/favicon2.jpg?']
+    $scope.Pics = ['img/favicons/favicon1.ico?', 'img/favicons/favicon2.ico?']
     $scope.var = 0
     $scope.chosenPic = $scope.Pics[$scope.var]
     $scope.callAtInterval = function() {
