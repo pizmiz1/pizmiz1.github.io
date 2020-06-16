5b6167a8e58e4137dccdca34ea5d6c854a68e5a9
Ethan <ebritton12321@gmail.com>
2020-06-16 01:13:36 -0400

    Added favicon and decreased interval time

--- IndexJs/index.customlink.js
+++ IndexJs/index.customlink.js
@@ -2,18 +2,18 @@ var MyApp = angular.module('Index');
 
 MyApp.controller('customlink', ['$scope', '$interval', function($scope, $interval){
 
-    $scope.Pics = ['img/favicons/favicon1.ico?', 'img/favicons/favicon2.ico?']
+    $scope.Pics = ['img/favicons/favicon1.ico?', 'img/favicons/favicon2.ico?', 'img/favicons/favicon3.ico?']
     $scope.var = 0
     $scope.chosenPic = $scope.Pics[$scope.var]
     $scope.callAtInterval = function() {
-        if ($scope.var == 0){
-            $scope.var = 1;
+        if ($scope.var == $scope.Pics.length - 1){
+            $scope.var = 0;
         }
         else {
-            $scope.var = 0;
+            $scope.var++;
         }
         $scope.chosenPic = $scope.Pics[$scope.var];
     }
 
-    $interval( function(){ $scope.callAtInterval(); }, 500);
+    $interval( function(){ $scope.callAtInterval(); }, 200);
 }]);
