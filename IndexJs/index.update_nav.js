1f92685b162fa2def7b19073f7dc4b9d1d645798
Ethan <ebritton12321@gmail.com>
2020-08-10 03:00:37 -0400

    Fixing Index Logic

--- IndexJs/index.update_nav.js
+++ IndexJs/index.update_nav.js
@@ -1,6 +1,6 @@
 var DanceApp = angular.module('Index');
 
-DanceApp.controller('update_nav_controller', function($scope, $sce){
+DanceApp.controller('update_nav_controller', function($scope){
     $scope.text = 'nav-item active';
     $scope.text2 = 'nav-item';
 });
